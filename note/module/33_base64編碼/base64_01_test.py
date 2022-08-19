f28ce5ae51aa9672845125c3581db2370f174f90
rogers Lu <61651257+rogers228@users.noreply.github.com>
2022-08-19 14:18:31 +0800

    Create base64_01_test.py

---
+++ note/module/33_base64編碼/base64_01_test.py
@@ -0,0 +1,14 @@
+﻿import base64
+
+def test1():
+    s = 'Hello World你好' # 源字串
+    print(s)
+
+    b = s.encode('UTF-8') # 轉碼為utf-8
+    print(b)
+
+    bytes_encode = base64.b64encode(b) # 轉碼為base64
+    print(bytes_encode)
+
+if __name__ == '__main__':
+    test1()
