0a28efa16f210a48ad72272ec3b04387e4509188
rogers Lu <61651257+rogers228@users.noreply.github.com>
2022-08-19 14:12:02 +0800

    新增

--- note/module/32_dirsync_檔案同步/dirsync01_test/main.py
+++ note/module/32_dirsync_檔案同步/dirsync01_test/main.py
@@ -4,10 +4,13 @@ import dirsync
 def main():
     source_path = r'C:\Users\user\Documents\GitHub\Learn_python\note\module\32_dirsync_檔案同步\dirsync01_test\sourcedir'
     target_path = r'C:\Users\user\Documents\Rogers\Temp\sync_test'
-    dirsync.sync(source_path, target_path, action='sync')
 
-    # dirsync.sync(config_develop_program, config_servr_program, action='sync', purge = True)
-    # purge = True 同步清除不存在的檔案
+    args = {
+        'purge': True,   # 同步清除
+        'create' : True, # 資料夾不存在時則建立
+        'ignore' : ['\.git', '\.gitignore', 'config.py', 'test.*'] # 忽略
+    }
+    dirsync.sync(source_path, target_path, 'sync', **args)
     print('finish')
 
 if __name__ == '__main__':
