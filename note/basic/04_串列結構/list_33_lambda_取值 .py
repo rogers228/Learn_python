62376431dee1110e6fdacd3a5c3bb7277924e418
rogers Lu <61651257+rogers228@users.noreply.github.com>
2022-08-22 17:30:17 +0800

    新增

---
+++ note/basic/04_串列結構/list_33_lambda_取值 .py
@@ -0,0 +1,10 @@
+def test5():
+
+    lis = ['20220822073547','20220822083014']
+    print(lis)
+
+    lis2 = list(map(lambda e: e[8:12], lis))
+    print(lis2)
+
+if __name__ == '__main__':
+    test5()
