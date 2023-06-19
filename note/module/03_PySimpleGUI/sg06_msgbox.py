45970005f0defc343731ddb4054880f8fd63be8e
rogers Lu <61651257+rogers228@users.noreply.github.com>
2023-06-19 09:34:16 +0800

    新增

--- note/module/03_PySimpleGUI/sg06_msgbox.py
+++ note/module/03_PySimpleGUI/sg06_msgbox.py
@@ -2,12 +2,11 @@ import PySimpleGUI as sg
 
 def test1():
     sg.theme('SystemDefault')
-    sg.popup_error('popup_error')  # Shows red error button
+    sg.popup_error('popup_error\nthe second')  # Shows red error button
 
 def test2():
     sg.theme('SystemDefault')
     sg.popup('\nPart number is not exist\nplease confirm whether your product number.\n\n', title='title')
 
-
-if __name__ == '__main__':  
+if __name__ == '__main__':
     test1()
