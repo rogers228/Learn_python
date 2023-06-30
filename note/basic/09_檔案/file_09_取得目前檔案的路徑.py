6f0aeeb8a8d724d11389fa1ffdc9e3d516feb534
rogers Lu <61651257+rogers228@users.noreply.github.com>
2023-06-30 16:12:01 +0800

    新增

--- note/basic/09_檔案/file_09_取得目前檔案的路徑.py
+++ note/basic/09_檔案/file_09_取得目前檔案的路徑.py
@@ -4,3 +4,6 @@ print(__file__) # fullpath
 print(os.path.dirname(__file__)) #fulldir
 print(os.path.dirname(os.path.dirname(__file__))) #fulldir's up
 
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.path.pardir))) #加入上層目錄的路徑
+sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..'))) #加入上層目錄的路徑  windows 與 linux 通用
