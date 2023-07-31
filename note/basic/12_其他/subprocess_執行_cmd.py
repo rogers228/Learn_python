d39502e8fe8b9b7d4ef4db72a2f2e9b0e2dad323
rogers Lu <61651257+rogers228@users.noreply.github.com>
2023-07-31 09:48:47 +0800

    新增

--- note/basic/12_其他/subprocess_執行_cmd.py
+++ note/basic/12_其他/subprocess_執行_cmd.py
@@ -9,3 +9,12 @@ except:
 
 import os
 os.system()
+
+
+
+
+# 確保執行
+result1 = subprocess.run(command1, capture_output=True, text=True)
+result1.check_returncode()  # 確保 command1 運行成功
+if result1.returncode == 0: # 確保 command1 運行成功
+    print('result1 is run finished')
