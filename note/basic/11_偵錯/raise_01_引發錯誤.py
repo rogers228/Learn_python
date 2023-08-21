072618e4411548c34b2f78437b2d3c4404bdd54f
rogers Lu <61651257+rogers228@users.noreply.github.com>
2023-08-21 14:55:44 +0800

    新增

---
+++ note/basic/11_偵錯/raise_01_引發錯誤.py
@@ -0,0 +1,5 @@
+if not isinstance(payload, dict):
+    raise TypeError("Variable must be a dictionary")
+
+
+# 當 payload 不是dict 引發錯誤
--- note/basic/11_偵錯/try_02_顯示錯誤訊息.py
+++ note/basic/11_偵錯/try_02_顯示錯誤訊息.py
@@ -7,5 +7,13 @@ def main():
         traceback.print_exc()  # 將顯示完整錯誤，如同編譯錯誤顯示
 
 
+def test1(): # 檢查型別 引發錯誤
+    payload = 1 # not dict
+    try:
+        if not isinstance(payload, dict):
+            raise TypeError("Variable must be a dictionary")
+    except Exception as e:
+        traceback.print_exc()  # 將顯示完整錯誤，如同編譯錯誤顯示
+
 if __name__ == '__main__':
-    main()
+    test1()
