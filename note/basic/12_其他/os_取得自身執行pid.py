494ef61545bc946e2ba105483b662beba204c263
rogers Lu <61651257+rogers228@users.noreply.github.com>
2023-08-30 09:23:49 +0800

    新增

---
+++ note/basic/12_其他/os_取得自身執行pid.py
@@ -0,0 +1,6 @@
+import os
+
+# 獲取自身的進程ID（PID）
+current_pid = os.getpid()
+
+print("自身的進程ID（PID）:", current_pid)
---
+++ note/basic/12_其他/sys02_取得自身執行的解釋器位置.py
@@ -0,0 +1,6 @@
+import sys
+
+# 獲取Python解釋器的路徑
+interpreter_path = sys.executable
+
+print("Python解釋器的路徑:", interpreter_path)
