891be8265a6ae25bcb4f82dba20e5321b5a9a865
rogers Lu <61651257+rogers228@users.noreply.github.com>
2023-11-14 13:23:35 +0800

    新增

--- note/basic/04_dictionary/dic_05_判斷是否為字典.py
+++ note/basic/04_dictionary/dic_05_判斷是否為字典.py
@@ -1,6 +1,17 @@
-def test3():
+import json
+def test1():
     a = {5:5}
     if isinstance(a, dict):
         print('a is dic')
     else:
         print('a is not dic')
+
+def test2():
+    str_kwargs = '{"message": "yeohse"}'
+    dic_kwargs = json.loads(str_kwargs)
+    if not isinstance(dic_kwargs, dict):
+        print('type error! dic_kwargs is not dictionary')
+    print('ok')
+
+if __name__ == '__main__':
+    test2()
