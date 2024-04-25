ca4ea3568a5c88fb2f0c88ee2f5c6d55411a28d3
rogers Lu <61651257+rogers228@users.noreply.github.com>
2024-04-25 11:30:10 +0800

    新增

---
+++ note/basic/09_檔案/file_19_獲取檔案列表.py
@@ -0,0 +1,2 @@
+from glob import glob
+files_scss = glob(os.path.join(_p(__file__), "*.scss"))
--- note/module/18_jinja2_html渲染/jin01_test1.py
+++ note/module/18_jinja2_html渲染/jin01_test1.py
@@ -5,6 +5,14 @@ def render_jinja_html(template_loc,file_name,**context):
         loader=jinja2.FileSystemLoader(template_loc+'/')
     ).get_template(file_name).render(context)
 
+
+# 直接讀取temp文字內容後帶入，更直觀
+# def render_jinja_html(temp_content, **context):
+#     return jinja2.Environment(
+#         loader=jinja2.BaseLoader()
+#     ).from_string(temp_content).render(context)
+
+
 def test1():
     # 渲染
     h = render_jinja_html(r'C:\Users\user\Documents\GitHub\Learn_python\note\module\18_jinja2_html渲染',
@@ -15,4 +23,4 @@ def test1():
     # 成功渲染! 這樣一來html可以獨立為view模板 邏輯層由python負責
 
 if __name__ == '__main__':
-    test1()    
+    test1()
