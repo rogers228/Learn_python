6b7988cfdfc77bbbc50c594e36336a5b2fac3128
rogers Lu <61651257+rogers228@users.noreply.github.com>
2024-07-11 15:56:55 +0800

    新增

--- note/basic/05_函數/func_08_不限定數量的引數args.py
+++ note/basic/05_函數/func_08_不限定數量的引數args.py
@@ -1,8 +1,20 @@
-def test_args(*args):
-    s = 0
-    for arg in args:
-        s += arg
-    return s
+# def test_args(*args):
+#     s = 0
+#     for arg in args:
+#         s += arg
+#     return s
 
-def test41():
-    print(test_args(1,2,3,4,5))
+
+
+def test_args(*args):
+    for i, argv in enumerate(args):
+        print(i, argv)
+
+
+
+def test1():
+    # print(test_args(1,2,3,4,5))
+    print(test_args('a', 'b', 'c'))
+
+if __name__ == '__main__':
+    test1()
