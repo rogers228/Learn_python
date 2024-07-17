98c1c864f15b5d28fe0310fa70088378f2342ece
rogers Lu <61651257+rogers228@users.noreply.github.com>
2024-07-17 15:56:40 +0800

    新增

--- note/module/32_dirsync_檔案同步/dirsync01_test/main.py
+++ note/module/32_dirsync_檔案同步/dirsync01_test/main.py
@@ -2,16 +2,17 @@
 import dirsync
 
 def main():
-    source_path = r'C:\Users\USER\Documents\learn-front-end\test_code\20230121_react\11_template'
-    target_path = r'C:\Users\USER\Documents\learn-front-end\test_code\20230121_react\46_web\template'
+    source_path = r'C:\Users\user\Documents\GitHub\Learn_python\note\module\32_dirsync_檔案同步\dirsync01_test\sourcedir'
+    target_path = r'C:\Users\user\Documents\GitHub\Learn_python\note\module\32_dirsync_檔案同步\dirsync01_test\targetdir'
 
     args = {
         'purge': True,   # 同步清除
         'create' : True, # 資料夾不存在時則建立
-        'ignore' : ['\.git', '\.gitignore', 'readme.md', 'push_to_webserver.py', r'.*\.log', '__pycache__'] # 忽略
+        'ignore' : ['\.git', '\.gitignore', 'readme.md', 'push_to_webserver.py', r'.*\.log', '__pycache__'], # 忽略
         # \. 代表 .
         # \* 代表 *
         # \\ 代表 \
+        # */ 代表底下所有資料夾
         # __pycache__ 代表 資料夾__pycache__
     }
 
