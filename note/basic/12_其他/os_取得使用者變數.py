8f0cd88d5503988bfdf84ea87e1e9c67fd00a7d4
rogers Lu <61651257+rogers228@users.noreply.github.com>
2024-11-18 15:07:55 +0800

    新增

--- note/basic/12_其他/os_取得使用者變數.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def test1():
-    user_variable = os.environ.get('TEST')
-    print(user_variable)
-
-if __name__ == '__main__':
-    test1()
---
+++ note/basic/12_其他/version_02_當前執行版本.py
@@ -0,0 +1,8 @@
+import sys
+
+def test1():
+    # 打印當前 Python 版本
+    print(f"當前 Python 版本: {sys.version}")
+
+if __name__ == '__main__':
+    test1()
