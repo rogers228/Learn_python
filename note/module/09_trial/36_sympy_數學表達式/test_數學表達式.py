fdc82cba78389e46e205653fa56a3d1b1e4da726
rogers228 <61651257+rogers228@users.noreply.github.com>
2025-04-12 10:59:57 +0800

    新增

--- note/module/09_trial/36_sympy_數學表達式/test_數學表達式.py
+++ note/module/09_trial/36_sympy_數學表達式/test_數學表達式.py
@@ -1,9 +1,23 @@
-from sympy import symbols, sqrt, latex
-
-x = symbols('x')
-# expr = sqrt(x) + x**2 + 1/x
-expr = x**2 + 2*x + 1
-latex_expr = latex(expr)
-print(f"$$\n{latex_expr}\n$$")
-
+# 數學表達式
 ## 貼上 https://stackedit.io/app# 就可產生 數學式
+
+from sympy import (symbols,  latex,
+    sqrt, sin, limit)
+
+def test1():
+    x = symbols('x') # 定義一個符號變數 x，可以用來建立代數運算式。
+    expr = sqrt(x) + x**2 + 1/x
+    # expr = x**2 + 2*x + 1
+    latex_expr = latex(expr) # 定義一個符號變數 x，可以用來建立代數運算式。
+    print(f"$${latex_expr}$$")
+
+def test2():
+    x, a = symbols('x a')
+    f = sin(x)/x
+    lim_expr = limit(f, x, a)
+
+    latex_code = latex(limit(f, x, a)) + " = " + latex(lim_expr)
+    print(f"$$\n{latex_code}\n$$")
+
+if __name__ == '__main__':
+    test2()
